34308788b14d65501e3ab98d2c177ef8a6443bdf
Basel Mousa Atalla <baselatalla10@gmail.com>
2021-05-04 23:33:13 +0300

    done labe13

--- server.js
+++ server.js
@@ -9,24 +9,24 @@ const server = express();
 
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL);
+const methodOverride = require('method-override');
 server.set('view engine','ejs');
 
 server.use(express.urlencoded({extended:true}));
 server.use(express.static(__dirname + '/public'));
+server.use(methodOverride('_method'));
 
 server.get('/', homehandler);
-
 server.get('/hello',(req,res)=>{
   res.render('pages/index');
 });
 
-server.get('/searches/new', (req,res)=>{
-  res.render('pages/searches/new');
-});
+server.get('/searches/new', (req,res)=>{ res.render('pages/searches/new');});
 server.post('/searches', searchHandler);
-
 server.post('/books', addHandler);
-server.get('/bookDetail/:bookID',getOnebookDetails);
+server.get('/bookDetail/:bookID', getOnebookDetails);
+server.put('/updateBook/:bookID', updateBookHandler);
+server.delete('/deleteBook/:bookID', deleteBookHandler);
 
 function searchHandler(req,res){
   let searchValue = req.body.search;
@@ -44,22 +44,12 @@ function searchHandler(req,res){
 }
 
 function addHandler(req,res){
-  // let SQL = `SELECT * FROM books ;`;
-  // client.query(SQL)
-  //   .then(result=>{
-  //     result.rows.forEach((n)=>{
-  //       console.log(n);
-  //       if(n.title === req.body.title && n.authors === req.body.authors){
-  //         res.redirect(`/bookDetail/${n.id}`);
-  //       }else {
   let SQL1 = `INSERT INTO books (authors,title,isbn,image,description) VALUES($1,$2,$3,$4,$5) RETURNING *;`;
   let safevalues = [req.body.authors, req.body.title, req.body.isbn, req.body.image, req.body.description];
   client.query(SQL1, safevalues)
     .then(result => {
       res.redirect(`/bookDetail/${result.rows[0].id}`);
     });
-  //  });
-  // });
 }
 
 function getOnebookDetails(req,res){
@@ -72,14 +62,7 @@ function getOnebookDetails(req,res){
     });
 }
 
-function homehandler(req,res){
-  let SQL=`SELECT * FROM books `;
-  client.query(SQL)
-    .then(result => {
-      // console.log(result)
-      res.render('pages/index', { book: result.rows });
-    });
-}
+
 
 function BooksData(data){
   this.title = data.volumeInfo.title || '* not available*';
@@ -96,6 +79,34 @@ function BooksData(data){
 
 }
 
+
+function updateBookHandler(req,res){
+  let {authors,title,isbn,image,description} = req.body;
+  let SQL = `UPDATE books SET authors=$1,title=$2,isbn=$3,image=$4,description=$5 WHERE id=$6;`;
+  let safeValues = [authors,title,isbn,image,description,req.params.bookID];
+  console.log(req.params.bookID);
+  client.query(SQL,safeValues)
+    .then(()=>{
+      res.redirect(`/bookDetail/${req.params.bookID}`);
+    });
+}
+
+function deleteBookHandler(req,res){
+  let SQL=`DELETE FROM books WHERE id=$1;`;
+  let value=[req.params.bookID];
+  client.query(SQL,value)
+    .then(()=>{
+      res.redirect('/');
+    });
+}
+
+function homehandler(req,res){
+  let SQL=`SELECT * FROM books `;
+  client.query(SQL)
+    .then(result => {
+      res.render('pages/index', { book: result.rows });
+    });
+}
 server.get('*',(req,res)=>{
   res.render('pages/error');
 });
